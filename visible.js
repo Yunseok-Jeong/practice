52c8f35ac130f39178d7668cf016d27029348e1b
Yunseok-Jeong <76703841+Yunseok-Jeong@users.noreply.github.com>
2020-12-31 02:19:19 +0900

    Add files via upload

---
+++ visible.js
@@ -0,0 +1,17 @@
+const toggleBtn = document.querySelector('.navbar_toggleBtn');
+const menu = document.querySelector('.navbarmenu');
+var a=1;
+
+toggleBtn.addEventListener('click', ()=>{
+    if(a==1){
+        menu.style.display='flex';
+        a=2;
+    }
+    else{
+        menu.style.display='none';
+        a=1;
+    }
+});
+menu.addEventListener('click', ()=>{
+    menu.style.display='none';
+});
